25f6fa876f66e9d79c6525992f6a19bb1b54dfd9
wagner <wagner.santos@accurate.com.br>
2023-04-11 10:26:45 -0300

    Testes refatorados com Design Pattern Strategy

--- projeto-inicial/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
+++ projeto-inicial/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -1,8 +1,26 @@
 package main.java.br.com.alura.tdd.modelo;
 
-public enum Desempenho {
-    A_DESEJAR,
-    BOM,
-    OTIMO;
+import java.math.BigDecimal;
 
+public enum Desempenho {
+    A_DESEJAR {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.03");
+        }
+    },
+    BOM {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.15");
+        }
+    },
+    OTIMO {
+        @Override
+        public BigDecimal percentualReajuste() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal percentualReajuste();
 }
--- projeto-inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ projeto-inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,18 +8,8 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        BigDecimal salario = funcionario.getSalario();
-        if (desempenho == Desempenho.A_DESEJAR) {
-            BigDecimal reajuste = salario.multiply(new BigDecimal("0.03"));
-            funcionario.reajustarSalario(reajuste);
-        }
-        if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste = salario.multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        }
-        if (desempenho == Desempenho.OTIMO) {
-            BigDecimal reajuste = salario.multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste);
-        }
+        BigDecimal percentual = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+        funcionario.reajustarSalario(reajuste);
     }
 }
